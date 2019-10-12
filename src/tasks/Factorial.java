9f9daed68a3794a648847135bfad299a91b12882
Vitaliy <movcan77@gmail.com>
2019-10-12 12:48:08 +0300

    Added methods for finding the factorial of a given number and outputting the number of Fibonacci given sequence length

---
+++ src/tasks/Factorial.java
@@ -0,0 +1,29 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Factorial {
+	
+	private static void factorial(int startNumber) {
+
+		long factorial = 1;
+
+		for (int i = 1; i <= startNumber; i++) {
+			factorial *= i;
+		}
+
+		System.out.print(startNumber + "! = " + factorial);
+	}
+
+    public static void main(String[] args) {
+
+        System.out.print("Enter number for calculate factorial: ");
+        
+        Scanner scanData = new Scanner(System.in);
+        int startNumber = scanData.nextInt();
+
+        factorial(startNumber);
+
+    }
+
+}
---
+++ src/tasks/Fibonachi.java
@@ -0,0 +1,52 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Fibonachi {
+
+	private static void firstVersion(int count) {
+
+		long[] arrayFIb = new long[count + 2];
+		arrayFIb[0] = 0;
+		arrayFIb[1] = 1;
+
+		for (int i = 2; i <= count + 1; i++) {
+
+			arrayFIb[i] = arrayFIb[i - 1] + arrayFIb[i - 2];
+
+			System.out.print(arrayFIb[i - 2] + " ");
+		}
+		System.out.println("");
+	}
+
+	private static void secondVersion(int count) {
+
+		long[] arrayFIb = new long[count + 2];
+
+		for (int i = 0; i <= count; i++) {
+
+			if (i < 2) {
+				arrayFIb[i] = 1;
+			} else {
+				arrayFIb[i] = arrayFIb[i - 1] + arrayFIb[i - 2];
+			}
+			System.out.print(arrayFIb[i] + " ");
+			
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.print("Enter number: ");
+
+		Scanner scanData = new Scanner(System.in);
+		int count = scanData.nextInt();
+
+		firstVersion(count);
+		secondVersion(count);
+		
+		scanData.close();
+	}
+
+}
